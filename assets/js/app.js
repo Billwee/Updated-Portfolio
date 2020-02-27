b3d8c3d1dd3f77f0891bf3c470d8f5221787be2e
Bill Kamberos <bill.kamberos@gmail.com>
2020-02-27 18:49:55 -0500

    REmoved non-working nav-bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,24 @@
-$(document).ready(function() {
-  var $nav = $('#main-nav2'); //Caching element
-  // hide .navbar first - you can also do this in css .nav{display:none;}
-  $nav.hide();
+//Sticky nav bar that appears when scrolling
+// Couldn't get it to work properly with media queries
+// $(document).ready(function() {
+//   var $nav = $('#nav-box');
 
-  // fade in .navbar
-  $(function() {
-    $(window).scroll(function() {
-      // set distance user needs to scroll before we start fadeIn
-      if ($(this).scrollTop() > 50) {
-        //For dynamic effect use $nav.height() instead of '100'
-        $nav.fadeIn();
-      } else {
-        $nav.fadeOut();
-      }
-    });
-  });
-});
+//   $(function() {
+//     $(window).scroll(function() {
+//       // set distance user needs to scroll before we start fadeIn
+//       if ($(this).scrollTop() > 560) {
+//         //For dynamic effect use $nav.height() instead of '100'
+//         $nav.css('position', 'fixed');
+//         $nav.css(
+//           'background',
+//           "url('../img/back2.jpg') no-repeat center right/cover"
+//         );
+//       } else {
+//         $nav.css('position', 'relative');
+//       }
+//     });
+//   });
+// });
 
+//Smooth Scrolling
 var scroll = new SmoothScroll('a[href*="#"]');
